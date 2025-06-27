51f35b3449e1083807c40fcb44c1fa8966a4be5d
BheemChand1 <BHEEMCHAND.21711277@gehu.ac.in>
2025-06-28 01:49:15 +0530

    night push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,53 +62,7 @@
     <!--============================
         MENU START
     =============================-->
-    <nav class="navbar navbar-expand-lg main_menu">
-        <div class="container">
-            <a class="navbar-brand" href="index.html">
-                <img src="images/logo.png" alt="Freeit" class="img-fluid">
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <i class="far fa-bars"></i>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav m-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">About</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Services</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Portfolio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Shop</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="blogs.html">Blog</a>
-                    </li>
-                </ul>
-                <ul class="right_menu d-flex flex-wrap align-items-center">
-                    <li>
-                        <a href="#" class="wsus__manu_cart icon">
-                            <span>
-                                <img src="images/cart_icon_black.svg" alt="cart" class="img-fluid">
-                                <b>2</b>
-                            </span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="common_btn">Let’s Talk</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    @include('layouts.header')
     <!--============================
         MENU END
     =============================-->
@@ -126,25 +80,7 @@
     <!--============================
         FOOTER START
     =============================-->
-    <footer class="pt_100 pb_100">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="footer_text">
-                        <a class="footer_logo" href="index.html">
-                            <img src="images/logo.png" alt="Freeit" class="img-fluid">
-                        </a>
-                        <ul class="d-flex flex-wrap">
-                            <li><a href="#"><i class="fab fa-twitter" aria-hidden="true"></i></a></li>
-                            <li><a href="#"><i class="fab fa-facebook-f" aria-hidden="true"></i></a></li>
-                            <li><a href="#"><i class="fab fa-linkedin-in" aria-hidden="true"></i></a></li>
-                        </ul>
-                        <p>Copyright © 2025 All Rights Reserved by Freeit</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+    @include('layouts.footer')
     <!--============================
         FOOTER END
     =============================-->
